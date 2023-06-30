7a6b0f5dc3b394128edc08734a0ff63e4afff109
Dmitriy Vasilkov <dmitriy.v@tlight.biz>
2023-06-30 18:47:59 +0200

    change project structure

---
+++ db/session.py
@@ -0,0 +1,26 @@
+from typing import Generator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+import settings
+
+
+# create async engine for interaction with database
+engine = create_async_engine(settings.DATABASE_URL, future=True, echo=True)
+
+# create session for the interaction with database
+async_session = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+
+async def get_db() -> Generator:
+    try:
+        session: AsyncSession = async_session()
+        yield session
+    finally:
+        session.close()
